b5320ba143a81c5f7f5bb43447ad5447b33ea294
Brian Zeligson <bzeligson@localytics.com>
2014-11-23 18:56:18 -0500

    fix mailer

--- app/mailers/bumper_mailer.rb
+++ app/mailers/bumper_mailer.rb
@@ -10,7 +10,10 @@ class BumperMailer < ActionMailer::Base
   def return_reminder(email)
     Rails.logger.info "Mailing reminder for #{email.to_s.inspect}"
     mail(to: email[:from], content_type: 'text/html',
-      subject: email[:subject], body: body_from(email))
+      subject: email[:subject]) do |format|
+        format.html { render inline: body_from(email) } 
+        format.text { render text: body_from(email) }
+    end
   end
 
   def how_to(recipient)
